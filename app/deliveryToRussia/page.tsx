1f00813e9822a75dd0ecc6a773bd56c00f2b9be2
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-11-01 13:40:36 +0600

    fix

---
+++ app/deliveryToRussia/page.tsx
@@ -0,0 +1,12 @@
+import { HeaderProfile } from '@/components/HeaderProfile/HeaderProfile'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <HeaderProfile />
+        </div>
+    )
+}
+
+export default page
