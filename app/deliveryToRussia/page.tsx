5af0cfc410aef9e4d7de5c596535224089635a08
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-11-01 13:52:45 +0600

    fix error

--- app/deliveryToRussia/page.tsx
+++ app/deliveryToRussia/page.tsx
@@ -1,10 +1,15 @@
 import { HeaderProfile } from '@/components/HeaderProfile/HeaderProfile'
 import React from 'react'
-
+import s from './page.module.scss'
 const page = () => {
     return (
         <div>
             <HeaderProfile />
+            <div className={`Contend ${s.header}`}>'
+                <h2>Оформить доставку по РФ</h2>
+                <p>Данная форма предназначена для оформления заявки
+                    на доставку из склада по Российской Федерации</p>
+            </div>
         </div>
     )
 }
