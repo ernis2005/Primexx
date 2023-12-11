6a1264b62b12a9155897f0ea07912d636bea2b6f
Aidar-hub <146725346+Aidar-hub@users.noreply.github.com>
2023-12-12 02:21:32 +0600

    kkln

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                         <h3><HeaderSvg /></h3>
                         <p>Дарим доступ к растущему <br />
                             миру возможностей! </p>
-                        <div className="flex">
+                        <div className={`${s.none} flex`}>
                             <Link href={'https://www.instagram.com/'}>
                                 <Image className={s.img} src={insta} alt='' />
                             </Link>
@@ -76,13 +76,48 @@ const Footer = () => {
                             </ul>
                         </div>
                         <div>
-                            <ul className={s.ul_bar}>
-                                <li >Горячая линия по вопросам  доставки</li>
+                            <ul className={s.ul_bars}>
+                               <div className={s.litop}>
+                               <li >Горячая линия по вопросам  доставки</li>
                                 <li>
                                     <button className={s.btn}>
-                                        Написать
+                                        Написать <span> WhatsApp</span>
                                     </button>
                                 </li>
+                               </div>
+                               <ul className={s.ul_barskan}>
+                                <li>
+                                    <Link href={'/'}>
+                                        Главная
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Page/services'}>
+                                        Услуги
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Page/rates'}>
+                                        Тарифы и сроки
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Page/news'}>
+                                        Новости
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Page/FAQ'}>
+                                        F.A.Q.
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Page/about_us'}>
+                                        О нас
+                                    </Link>
+                                </li>
+
+                            </ul>
                                 <li>Дополнительно</li>
                                 <li>
                                     <Link href={'/Page/about_us'}>
@@ -102,6 +137,26 @@ const Footer = () => {
                             </ul>
                         </div>
                     </div>
+                    <div className={`${s.block} between`}>
+                            <Link href={'https://www.instagram.com/'}>
+                                <Image className={s.img} src={insta} alt='' />
+                            </Link>
+                            <Link href={'https://vk.com/'}>
+                                <Image className={s.img} src={img} alt='' />
+                            </Link>
+                            <Link href={'https://www.tiktok.com/'}>
+                                <Image className={s.img} src={tikTok} alt='' />
+                            </Link>
+                            <Link href={'https://www.youtube.com/watch?v=oLPgYSNerik&list=RDMM&index=21'}>
+                                <Image className={s.img} src={youtube} alt='' />
+                            </Link>
+                            <Link href={'https://www.whatsapp.com/?lang=ru_RU'}>
+                                <Image className={s.img} src={what} alt='' />
+                            </Link>
+                            <Link href={'https://web.telegram.org/k/'}>
+                                <Image className={s.img} src={telegram} alt='' />
+                            </Link>
+                        </div>
                 </div>
             </div>
 
