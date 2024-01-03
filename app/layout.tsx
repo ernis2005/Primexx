adef83b5344e546256cb1ab27e3134fd91cf7afe
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-01-03 14:49:54 +0600

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,7 @@ import Login from '@/components/Login/Login'
 import { ReduxProider } from './redux/provider'
 import { StyledEngineProvider } from '@mui/material/styles';
 const inter = Inter({ subsets: ['latin'] })
-// export const metadata: Metadata = {
-//     title: 'ЭКСПРЕСС ДОСТАВКА ПОСЫЛОК ИЗ ТУРЦИИ И США',
-//     description: 'Сервис по доставке ваших любимых брендов из любого Американского и Турецкого сайта и маркетплейса в Россию. Бесплатный склад для байеров. Удобный личный кабинет ',
-//     icons: [
-//         {
-//             rel: "icon",
-//             sizes: "any",
-//             url: "/logo1.jpg",
-//         },
-//     ],
-// }
+
 export async function generateMetadata() {
     return {
         title: 'ЭКСПРЕСС ДОСТАВКА ПОСЫЛОК ИЗ ТУРЦИИ И США',
