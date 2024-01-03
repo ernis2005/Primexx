1d58d57b2031fad759e69cd84882ba05a4ba8956
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-01-03 13:39:53 +0600

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,5 @@
 import HomeComponent from "@/components/Home/Home";
 
-
-export const metadata = {
-    title: 'Primex',
-    description: 'Дарим доступ к растущему миру возможностей! ',
-    icons: [
-        {
-            rel: "icon",
-            sizes: "any",
-            url: "/logo1.jpg",
-        },
-    ],
-}
 const Home =  () => {
    
     return (<HomeComponent/>);
